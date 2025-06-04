25a11de1fa5390c3d94ddd84f0723134f8f29476
prasanna7codes <prasannalit098@gmail.com>
2025-06-04 13:19:45 +0530

    this front end does not store the history of messages , if we resfresh the messages are vanished

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,21 @@ import { useState,useRef, useEffect } from "react";
 
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
+import { Textarea } from "@/components/ui/textarea"
+import { ScrollArea } from "@/components/ui/scroll-area"
 
 function App() {
  
  const [roomId,setRoomId]= useState<string>();
   const [socket,setSocket]= useState<WebSocket>();
+  const[message ,setMessage]=useState<string>("");
+    const[messages ,setMessages]=useState<string[]>([])
+
 
 
 const roomRef = useRef<HTMLInputElement>(null);
+const textRef = useRef<HTMLTextAreaElement>(null);
+
 
 // first sending the value to backend , then doing  setRoomId(id) , bcoz if first doing setRoomId(id) and then sending roomId , it was sending a empty payload bcoz  roomId wasn't updated yet due to React state being asynchronous
 
@@ -35,6 +42,29 @@ function handlesubmit() {
 }
 
 
+function sendText(){
+
+  if(textRef.current){
+   const text= textRef.current.value;
+
+   if (socket && socket.readyState === WebSocket.OPEN) {
+      socket.send(
+        JSON.stringify({
+          type: "message",
+          payload: { message: text },
+        })
+      );
+    } else {
+      console.warn("Socket not ready yet");
+    }
+
+    setMessage(text);
+
+  }
+
+}
+
+
 
 
 useEffect(()=>{
@@ -42,7 +72,6 @@ useEffect(()=>{
 
     ws.onopen = () => {
     console.log("WebSocket connection opened");
-    setSocket(ws);
   };
 
     ws.onerror = (err) => {
@@ -50,22 +79,52 @@ useEffect(()=>{
   };
 
 
+  ws.onmessage = (ev) => {
+    try {
+      const data = JSON.parse(ev.data);
+      if (data.type === "message") {
+        const s = data.payload.message;
+        setMessages(prev => [...prev, s])
+      
+      }
+    } catch (error) {
+      console.error("Failed to parse WebSocket message:", error);
+    }
+  }
+
+      setSocket(ws);
+
+
+
 },[])
  
   return ( 
    <>
 
-
-
 <div className="bg-red-50 flex items-center">
   <Input ref={roomRef} className="max-w-xs mr-6" type="text" placeholder="roomId"/>
 <Button className="bg-yellow-200" onClick={handlesubmit}>submit</Button>
 
-
 {roomRef.current && (
-  <span>{roomRef.current.value}</span>
+  <span>you have joined the room { roomId}</span>
 )}
 </div>
+
+
+ //we will put messages inside the scroll are  here
+ <ScrollArea className="h-[200px] w-[350px] rounded-md border p-4">
+  {messages.map((msg, idx) => (
+    <div key={idx} className="mb-2">
+      {msg}
+    </div>
+  ))}
+</ScrollArea>
+
+
+
+
+<Textarea ref={textRef} className="max-w-xl mt-6" />
+<Button className="bg-yellow-200" onClick={sendText}>submit</Button>
 
 
    </>
